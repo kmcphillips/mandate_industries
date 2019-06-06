4e603417d1fc7feed602953f3956d06534c2ee96
Kevin McPhillips <github@kevinmcphillips.ca>
2019-06-06 12:13:25 -0400

    Extract base twilio operation

--- app/operations/twilio/answer_operation.rb
+++ app/operations/twilio/answer_operation.rb
@@ -1,30 +1,12 @@
 # frozen_string_literal: true
 module Twilio
-  class AnswerOperation < ApplicationOperation
-    input :params, accepts: Hash, type: :keyword, required: true
-
-    before do
-      halt(hangup_response) unless valid_webhook?
-    end
-
+  class AnswerOperation < Twilio::BaseOperation
     def execute
       response = Twilio::TwiML::VoiceResponse.new do |response|
         response.say(message: "Hello, and thank you for calling Mandate Industries Incorporated! Goodbye.")
       end
 
       response.to_s
-    end
-
-    private
-
-    def hangup_response
-      response = Twilio::TwiML::VoiceResponse.new
-      response.hangup
-      response.to_s
-    end
-
-    def valid_webhook?
-      params["AccountSid"] == Rails.application.credentials.twilio![:account_sid]
     end
   end
 end
---
+++ app/operations/twilio/base_operation.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+module Twilio
+  class BaseOperation < ApplicationOperation
+    input :params, accepts: Hash, type: :keyword, required: true
+
+    before do
+      halt(hangup_response) unless valid_webhook?
+    end
+
+    private
+
+    def hangup_response
+      response = Twilio::TwiML::VoiceResponse.new
+      response.hangup
+      response.to_s
+    end
+
+    def valid_webhook?
+      params["AccountSid"] == Rails.application.credentials.twilio![:account_sid]
+    end
+  end
+end
