4f97e6d5471dc32a5f87871eb8c93c8a46550710
Kevin McPhillips <github@kevinmcphillips.ca>
2019-06-06 13:37:25 -0400

    Link the encrypted secret key

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :rbenv_ruby, "2.6.0"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml'
+append :linked_files, 'config/database.yml', 'config/credentials/production.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/.well-known'
