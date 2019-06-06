ac7284799476b5853350a5fb761604d21e479e9b
Kevin McPhillips <github@kevinmcphillips.ca>
2019-06-06 13:01:39 -0400

    Deploy with capistrano

--- Gemfile
+++ Gemfile
@@ -1,11 +1,10 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.3'
+ruby '2.6.0'
 
 gem 'rails', '~> 6.0.0.rc1'
-# gem 'mysql2', '>= 0.4.4'
-gem "sqlite3"
+gem 'mysql2', '>= 0.4.4'
 gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5'
 gem 'webpacker', '~> 4.0'
@@ -14,15 +13,24 @@ gem 'jbuilder', '~> 2.5'
 # gem 'image_processing', '~> 1.2'
 gem 'bootsnap', '>= 1.4.2', require: false
 
-gem "twilio-ruby"
-gem "active_operation"
+gem 'twilio-ruby'
+gem 'active_operation'
 
 group :development, :test do
-  gem "pry-rails"
-  gem "rspec-rails"
+  gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'sqlite3'
 end
 
 group :development do
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'capistrano', '3.6.1'
+  gem 'capistrano-rails'
+  gem 'capistrano-rbenv'
+  gem 'capistrano3-unicorn'
+end
+
+group :production do
+  gem 'unicorn'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'mandate.kev.cool'
+set :repo_url, 'https://github.com/kmcphillips/mandate_industries.git'
+
+set :user, "deploy"
+set :rbenv_ruby, "2.6.0"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+append :linked_files, 'config/database.yml'
+
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/.well-known'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 2
+
+# Unicorn
+# set :unicorn_pid, -> { File.join(current_path, "tmp", "pids", "unicorn.pid") }
+set :unicorn_config_path, -> { File.join(current_path, "config", "unicorn.rb") }
+# set :unicorn_roles, -> { :app }
+# set :unicorn_options, -> { "" }
+# set :unicorn_rack_env, -> { fetch(:rails_env) == "development" ? "development" : "deployment" }
+# set :unicorn_restart_sleep_time, 3
