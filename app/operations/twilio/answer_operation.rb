810c37ff8dab3b79ef78d32bffc0d31fa398c7d9
Kevin McPhillips <github@kevinmcphillips.ca>
2019-06-06 12:01:03 -0400

    Use twilio operations, credentials, and ngrok

--- app/operations/twilio/answer_operation.rb
+++ app/operations/twilio/answer_operation.rb
@@ -3,8 +3,28 @@ module Twilio
   class AnswerOperation < ApplicationOperation
     input :params, accepts: Hash, type: :keyword, required: true
 
+    before do
+      halt(hangup_response) unless valid_webhook?
+    end
+
     def execute
-      "<TODO></TODO>"
+      response = Twilio::TwiML::VoiceResponse.new do |response|
+        response.say(message: "Hello, and thank you for calling Mandate Industries Incorporated! Goodbye.")
+      end
+
+      response.to_s
+    end
+
+    private
+
+    def hangup_response
+      response = Twilio::TwiML::VoiceResponse.new
+      response.hangup
+      response.to_s
+    end
+
+    def valid_webhook?
+      params["AccountSid"] == Rails.application.credentials.twilio![:account_sid]
     end
   end
 end
