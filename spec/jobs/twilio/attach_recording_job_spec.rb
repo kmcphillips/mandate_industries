43a0299ebae7a793939ab1763e9c952db3f38202
Kevin McPhillips <github@kevinmcphillips.ca>
2019-07-06 14:36:23 -0400

    Add some job spec

--- spec/jobs/twilio/attach_recording_job_spec.rb
+++ spec/jobs/twilio/attach_recording_job_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Twilio::AttachRecordingJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:recording_id) { 1234 }
+
+  describe "#perform" do
+    it "calls the operation" do
+      expect(Twilio::Phone::AttachRecordingOperation).to receive(:call).with(recording_id: recording_id)
+      Twilio::AttachRecordingJob.perform_now(recording_id: recording_id)
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,17 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
+RSpec.configure do |config|
+  config.before(:each) do
+    Sidekiq::Worker.clear_all
+  end
+  config.after(:each) do
+    Sidekiq::Worker.clear_all
+  end
+end
+
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 begin
