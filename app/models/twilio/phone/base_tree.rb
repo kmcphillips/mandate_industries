e0a1c7223a5635c01cde26e071ef4b22a2433fad
Kevin McPhillips <github@kevinmcphillips.ca>
2019-07-06 15:18:32 -0400

    WIP DSL

--- app/models/twilio/phone/base_tree.rb
+++ app/models/twilio/phone/base_tree.rb
@@ -3,23 +3,28 @@ module Twilio
   module Phone
     class BaseTree
       class << self
-        def voice(voice_name)
-          tree.config[:voice] = voice_name
+        def voice(&block)
+          tree.config[:voice] = yield
           nil
         end
 
-        def timeout_message(message)
-          tree.config[:timeout_message] = message
+        def timeout_message(&block)
+          raise ArgumentError unless block_given?
+          tree.config[:timeout_message] = block
           nil
         end
 
-        def greeting(message: nil, prompt:)
-          tree.greeting = Twilio::Phone::Tree::After.new(message: message, prompt: prompt)
+        def greeting(&block)
+          context = AfterContext.new
+          context.instance_eval(&block)
+          tree.greeting = Twilio::Phone::Tree::After.new(**context.args)
           nil
         end
 
-        def prompt(prompt_name, message:, gather:, after:)
-          tree.prompts[prompt_name] = Twilio::Phone::Tree::Prompt.new(name: prompt_name, message: message, gather: gather, after: after)
+        def prompt(prompt_name, &block)
+          context = PromptContext.new
+          context.instance_eval(&block)
+          tree.prompts[prompt_name] = Twilio::Phone::Tree::Prompt.new(**context.args.merge(name: prompt_name))
           nil
         end
 
@@ -29,6 +34,61 @@ module Twilio
 
         def tree
           @tree ||= Twilio::Phone::Tree.new(tree_name)
+        end
+      end
+
+      class AfterContext
+        attr_reader :args
+
+        def initialize
+          @args = {
+            message: nil,
+            prompt: nil,
+            hangup: nil,
+          }
+        end
+
+        def message(val=nil, &block)
+          @args[:message] = val || block
+          nil
+        end
+
+        def prompt(val)
+          @args[:prompt] = val
+          nil
+        end
+
+        def hangup
+          @args[:hangup] = true
+        end
+      end
+
+      class PromptContext
+        attr_reader :args
+
+        def initialize
+          @args = {
+            message: nil,
+            gather: nil,
+            after: nil,
+          }
+        end
+
+        def message(val=nil, &block)
+          @args[:message] = val || block
+          nil
+        end
+
+        def gather(gather_args)
+          @args[:gather] = gather_args
+          nil
+        end
+
+        def after(&block)
+          context = AfterContext.new
+          context.instance_eval(&block)
+          @args[:after] = context.args
+          nil
         end
       end
     end
--- app/models/twilio/phone/favourite_number_tree.rb
+++ app/models/twilio/phone/favourite_number_tree.rb
@@ -1,57 +1,46 @@
 # frozen_string_literal: true
 class Twilio::Phone::FavouriteNumberTree < Twilio::Phone::BaseTree
-  voice "male"
+  voice { "male" }
 
-  timeout_message ->(response) { "We did not receive your response in time." }
+  timeout_message { |response| "We did not receive your response in time." }
 
-  greeting message: ->(response) { "Hello, and thank you for calling Mandate Industries Incorporated!" },
-    prompt: :favourite_number
+  greeting do
+    message { |response| "Hello, and thank you for calling Mandate Industries Incorporated!" }
+    prompt :favourite_number
+  end
 
-  prompt :favourite_number,
-    message: "Using the keypad on your touch tone phone, please enter your favourite number.",
-    gather: {
-      type: :digits,
-      timeout: 10,
-      number: 1,
-    },
-    after: {
-      prompt: :second_favourite_number,
-      message: ->(response) { "Thank you for your selection." }
-    }
+  prompt :favourite_number do
+    message "Using the keypad on your touch tone phone, please enter your favourite number."
+    gather type: :digits, timeout: 10, number: 1
+    after do
+      prompt :second_favourite_number
+      message { |response| "Thank you for your selection." }
+    end
+  end
 
-  prompt :second_favourite_number,
-    message: "In the case that your favourite number is not available, please enter your second favourite number.",
-    gather: {
-      type: :digits,
-      timeout: 10,
-      number: 1,
-    },
-    after: ->(response) {
+  prompt :second_favourite_number do
+    message "In the case that your favourite number is not available, please enter your second favourite number."
+    gather type: :digits, timeout: 10, number: 1
+    after do |response|
+      # TODO now this evaluates at the wrong time
       prev_fav = response.phone_call.responses.completed.find_by(prompt_handle: "favourite_number")
-
       if prev_fav.digits == response.digits
-        {
-          prompt: :second_favourite_number,
-          message: "Sorry, but your second favourite number cannot be the same as your first.",
-        }
+        prompt :second_favourite_number
+        message "Sorry, but your second favourite number cannot be the same as your first."
       else
-        {
-          prompt: :favourite_number_reason,
-          message: "Your favourite numbers have been recorded. The numbers #{response.digits} and #{prev_fav.digits}",
-        }
+        prompt :favourite_number_reason
+        message { |response| "Your favourite numbers have been recorded. The numbers #{response.digits} and #{prev_fav.digits}" }
       end
-    }
+    end
+  end
 
-  prompt :favourite_number_reason,
-    message: ->(response) { "Now, please state after the tone your reason for picking those numbers as your favourites." },
-    gather: {
-      type: :voice,
-      length: 4,
-      transcribe: true,
-    },
-    after: {
-      hangup: true,
-      message: "Thank you for your input! We have made a note of your reasons. Your opinion is important to us and will be disregarded.
+  prompt :favourite_number_reason do
+    message { |response| "Now, please state after the tone your reason for picking those numbers as your favourites." }
+    gather type: :voice, length: 4, transcribe: true
+    after do
+      message "Thank you for your input! We have made a note of your reasons. Your opinion is important to us and will be disregarded.
         Mandate Industries appreciates your business.",
-    }
+      hangup
+    end
+  end
 end
